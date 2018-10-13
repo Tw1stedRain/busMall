e742a1c18ccd07841ed07a0d6ac8bb008be2506a
Nicole Kalbfleisch <ni.ka.50@hotmail.com>
2018-10-13 11:22:40 -0700

    got all images rotating on clicks, stops at 25

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var currentMiddleImageArrayIndex = 10;
 var currentRightImageArrayIndex = 19;
 
 var allItems = [];
+var clickCounter = 0;
 
 //item constructor
 var Item = function (name, src) {
@@ -26,13 +27,6 @@ var Item = function (name, src) {
   allItems.push(this);
 };
 
-//prototypes
-
-// Item.prototype.renderItem = function (){
-//   itemImageLeft.src = this.src;
-// };
-
-// event listeners and handlers
 var ItemClickHandler = function (event) {
   if (event.target.id === 'left' || event.target.id === 'middle' || event.target.id === 'right') {
 
@@ -49,88 +43,74 @@ var ItemClickHandler = function (event) {
     } while (randomNumberRight === currentLeftImageArrayIndex || randomNumberRight === randomNumberLeft || randomNumberRight === currentRightImageArrayIndex);
 
 
-
-
-
-    if(event.target.id === 'left'){
+    if (event.target.id === 'left') {
       allItems[currentLeftImageArrayIndex].timesLiked++;
-    }else if (event.target.id === 'middle'){
+    } else if (event.target.id === 'middle') {
       allItems[currentMiddleImageArrayIndex].timesLiked++;
-    }else{
+    } else {
       allItems[currentRightImageArrayIndex].timesLiked++;
     }
 
     allItems[currentLeftImageArrayIndex].timesShown++;
     allItems[currentMiddleImageArrayIndex].timesShown++;
     allItems[currentRightImageArrayIndex].timesShown++;
-    console.log(allItems[currentRightImageArrayIndex]);
 
     currentLeftImageArrayIndex = randomNumberLeft;
     currentMiddleImageArrayIndex = randomNumberMiddle;
     currentRightImageArrayIndex = randomNumberRight;
 
-    event.target.src = allItems[randomNumberLeft].src;
-    event.target.src = allItems[randomNumberMiddle].src;
-    event.target.src = allItems[randomNumberRight].src;
+    itemImageLeft.src = allItems[randomNumberLeft].src;
+    itemImageMiddle.src = allItems[randomNumberMiddle].src;
+    itemImageRight.src = allItems[randomNumberRight].src;
 
     itemLeftText.textContent = allItems[randomNumberLeft].name;
     itemMiddleText.textContent = allItems[randomNumberMiddle].name;
     itemRightText.textContent = allItems[randomNumberRight].name;
+
+    clickCounter++;
+    if (clickCounter === 25) {
+      //render chart
+
+      imageSection.removeEventListener('click', ItemClickHandler);
+    }
   }
 
 };
 
 imageSection.addEventListener('click', ItemClickHandler);
 
-// var middleItemClickHandler = function (event) {
-//   do {
-//     var randomNumber = Math.floor(Math.random() * allItems.length);
-//   } while(randomNumber === itemImageMiddleArrayIndex);
-
-//   allItems[itemImageMiddleArrayIndex].timesLiked++;
-//   allItems[itemImageMiddleArrayIndex].timesShown++;
-
-//   itemImageMiddleArrayIndex = randomNumber;
-//   event.target.src = allItems[randomNumber].src;
-// };
-
-// itemImageMiddle.addEventListener('click', middleItemClickHandler);
-
-// var rightItemClickHandler = function (event) {
-//   do {
-//     var randomNumber = Math.floor(Math.random() * allItems.length);
-//   } while(randomNumber === itemImageRightArrayIndex);
-
-//   allItems[itemImageRightArrayIndex].timesLiked++;
-//   allItems[itemImageRightArrayIndex].timesShown++;
-
-//   itemImageRightArrayIndex = randomNumber;
-//   event.target.src = allItems[randomNumber].src;
-// };
-
-// itemImageRight.addEventListener('click', rightItemClickHandler);
-
 //========================================================================
 //all items (20 items)
 //========================================================================
-new Item ('bag', './img/bag.jpg');
-new Item ('banana', './img/banana.jpg');
-new Item ('bathroom', './img/bathroom.jpg');
-new Item ('boots', './img/boots.jpg');
-new Item ('breakfast', './img/breakfast.jpg');
-new Item ('bubblegum', './img/bubblegum.jpg');
-new Item ('chair', './img/chair.jpg');
-new Item ('cthulhu', './img/cthulhu.jpg');
-new Item ('dog-duck', './img/dog-duck.jpg');
-new Item ('dragon', './img/dragon.jpg');
-new Item ('pen', './img/pen.jpg');
-new Item ('pet-sweep', './img/pet-sweep.jpg');
-new Item ('scissors', './img/scissors.jpg');
-new Item ('shark', './img/shark.jpg');
-new Item ('sweep', './img/sweep.png');
-new Item ('tauntaun', './img/tauntaun.jpg');
-new Item ('unicorn', './img/unicorn.jpg');
-new Item ('usb', './img/usb.gif');
-new Item ('water can', './img/water-can.jpg');
-new Item ('wine glass', './img/wine-glass.jpg');
+new Item('bag', './img/bag.jpg');
+new Item('banana', './img/banana.jpg');
+new Item('bathroom', './img/bathroom.jpg');
+new Item('boots', './img/boots.jpg');
+new Item('breakfast', './img/breakfast.jpg');
+new Item('bubblegum', './img/bubblegum.jpg');
+new Item('chair', './img/chair.jpg');
+new Item('cthulhu', './img/cthulhu.jpg');
+new Item('dog-duck', './img/dog-duck.jpg');
+new Item('dragon meat', './img/dragon.jpg');
+new Item('pen', './img/pen.jpg');
+new Item('pet-sweep', './img/pet-sweep.jpg');
+new Item('scissors', './img/scissors.jpg');
+new Item('shark', './img/shark.jpg');
+new Item('sweep', './img/sweep.png');
+new Item('tauntaun', './img/tauntaun.jpg');
+new Item('unicorn', './img/unicorn.jpg');
+new Item('usb', './img/usb.gif');
+new Item('water can', './img/water-can.jpg');
+new Item('wine glass', './img/wine-glass.jpg');
 //========================================================================
+
+//=====================================
+//chart
+//=====================================
+
+
+
+
+
+
+
