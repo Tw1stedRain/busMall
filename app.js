ec8064fc972904e44925b53283810ad7411b1a36
Nicole Kalbfleisch <ni.ka.50@hotmail.com>
2018-10-20 09:48:35 -0700

    perfected my random image generator

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ var itemLeftText = document.getElementById('item-1-text');
 var itemMiddleText = document.getElementById('item-2-text');
 var itemRightText = document.getElementById('item-3-text');
 
-var currentLeftImageArrayIndex = 0;
-var currentMiddleImageArrayIndex = 10;
-var currentRightImageArrayIndex = 19;
+// var currentLeftImageArrayIndex = 0;
+// var currentMiddleImageArrayIndex = 10;
+// var currentRightImageArrayIndex = 19;
+var lastDisplayed = [];
 
 var allItems = [];
 var clickCounter = 0;
@@ -34,46 +35,61 @@ var Item = function (name, src) {
 var ItemClickHandler = function (event) {
   if (event.target.id === 'left' || event.target.id === 'middle' || event.target.id === 'right') {
 
-    do {
-      var randomNumberLeft = Math.floor(Math.random() * allItems.length);
-    } while (randomNumberLeft === currentLeftImageArrayIndex || randomNumberLeft === currentRightImageArrayIndex || randomNumberLeft === currentMiddleImageArrayIndex);
+    // do {
+    //   var randomNumberLeft = Math.floor(Math.random() * allItems.length);
+    // } while (randomNumberLeft === currentLeftImageArrayIndex || randomNumberLeft === currentRightImageArrayIndex || randomNumberLeft === currentMiddleImageArrayIndex);
 
-    do {
-      var randomNumberMiddle = Math.floor(Math.random() * allItems.length);
-    } while (randomNumberMiddle === currentLeftImageArrayIndex || randomNumberMiddle === randomNumberLeft || randomNumberMiddle === currentRightImageArrayIndex);
+    // do {
+    //   var randomNumberMiddle = Math.floor(Math.random() * allItems.length);
+    // } while (randomNumberMiddle === currentLeftImageArrayIndex || randomNumberMiddle === randomNumberLeft || randomNumberMiddle === currentRightImageArrayIndex);
 
-    do {
-      var randomNumberRight = Math.floor(Math.random() * allItems.length);
-    } while (randomNumberRight === currentLeftImageArrayIndex || randomNumberRight === randomNumberLeft || randomNumberRight === currentRightImageArrayIndex);
+    // do {
+    //   var randomNumberRight = Math.floor(Math.random() * allItems.length);
+    // } while (randomNumberRight === currentLeftImageArrayIndex || randomNumberRight === randomNumberLeft || randomNumberRight === currentRightImageArrayIndex);
 
-
-    if (event.target.id === 'left') {
-      allItems[currentLeftImageArrayIndex].timesLiked++;
-    } else if (event.target.id === 'middle') {
-      allItems[currentMiddleImageArrayIndex].timesLiked++;
-    } else {
-      allItems[currentRightImageArrayIndex].timesLiked++;
+    var randomNumberLeft = Math.floor(Math.random() * allItems.length);
+    var randomNumberMiddle = Math.floor(Math.random() * allItems.length);
+    var randomNumberRight = Math.floor(Math.random() * allItems.length);
+    
+    while(lastDisplayed.includes(randomNumberLeft) || lastDisplayed.includes(randomNumberMiddle) || lastDisplayed.includes(randomNumberRight) || randomNumberLeft === randomNumberMiddle || randomNumberMiddle === randomNumberRight || randomNumberRight === randomNumberLeft){
+      randomNumberLeft = Math.floor(Math.random() * allItems.length);
+      randomNumberMiddle = Math.floor(Math.random() * allItems.length);
+      randomNumberRight = Math.floor(Math.random() * allItems.length);
     }
-
-    allItems[currentLeftImageArrayIndex].timesShown++;
-    allItems[currentMiddleImageArrayIndex].timesShown++;
-    allItems[currentRightImageArrayIndex].timesShown++;
-
-    currentLeftImageArrayIndex = randomNumberLeft;
-    currentMiddleImageArrayIndex = randomNumberMiddle;
-    currentRightImageArrayIndex = randomNumberRight;
-
+      
+    lastDisplayed[0] = randomNumberLeft;
+    lastDisplayed[1] = randomNumberMiddle;
+    lastDisplayed[2] = randomNumberRight;
+      
+    if (event.target.id === 'left') {
+      allItems[randomNumberLeft].timesLiked++;
+    } else if (event.target.id === 'middle') {
+      allItems[randomNumberMiddle].timesLiked++;
+    } else {
+      allItems[randomNumberRight].timesLiked++;
+    }
+  
+    allItems[randomNumberLeft].timesShown++;
+    allItems[randomNumberMiddle].timesShown++;
+    allItems[randomNumberRight].timesShown++;
+  
+    // currentLeftImageArrayIndex = randomNumberLeft;
+    // currentMiddleImageArrayIndex = randomNumberMiddle;
+    // currentRightImageArrayIndex = randomNumberRight;
+  
     itemImageLeft.src = allItems[randomNumberLeft].src;
     itemImageMiddle.src = allItems[randomNumberMiddle].src;
     itemImageRight.src = allItems[randomNumberRight].src;
-
+  
     itemLeftText.textContent = allItems[randomNumberLeft].name;
     itemMiddleText.textContent = allItems[randomNumberMiddle].name;
     itemRightText.textContent = allItems[randomNumberRight].name;
 
-
     clickCounter++;
     if (clickCounter === 25) {
+
+      // localStorage.setItem
+
       var chartData = {
         labels: itemNames,
         datasets: [{
@@ -141,14 +157,14 @@ var ItemClickHandler = function (event) {
       };
 
 
-      var chart ={
+      var chart = {
         type: 'bar',
         data: chartData,
         options: chartOptions,
       };
 
       var renderChart = function () {
-        for(var i in allItems){
+        for (var i in allItems) {
           itemNames.push(allItems[i].name);
           itemLikes.push(allItems[i].timesLiked);
         }
@@ -169,38 +185,23 @@ imageSection.addEventListener('click', ItemClickHandler);
 //========================================================================
 //all items (20 items)
 //========================================================================
-new Item('bag', './img/bag.jpg');
-new Item('banana', './img/banana.jpg');
-new Item('bathroom', './img/bathroom.jpg');
-new Item('boots', './img/boots.jpg');
-new Item('breakfast', './img/breakfast.jpg');
-new Item('bubblegum', './img/bubblegum.jpg');
-new Item('chair', './img/chair.jpg');
+new Item('Droid bag', './img/bag.jpg');
+new Item('Banana Cutter', './img/banana.jpg');
+new Item('Bathroom Stand', './img/bathroom.jpg');
+new Item('Open Toed Boots', './img/boots.jpg');
+new Item('Full Breakfast Maker', './img/breakfast.jpg');
+new Item('Meaty Bubblegum', './img/bubblegum.jpg');
+new Item('Bubble chair', './img/chair.jpg');
 new Item('cthulhu', './img/cthulhu.jpg');
-new Item('dog-duck', './img/dog-duck.jpg');
+new Item('dog is duck', './img/dog-duck.jpg');
 new Item('dragon meat', './img/dragon.jpg');
 new Item('pen', './img/pen.jpg');
 new Item('pet-sweep', './img/pet-sweep.jpg');
-new Item('scissors', './img/scissors.jpg');
+new Item('Pizza scissors', './img/scissors.jpg');
 new Item('Sharky Sleepy', './img/shark.jpg');
-new Item('sweep', './img/sweep.png');
+new Item('Baby sweep', './img/sweep.png');
 new Item('tauntaun', './img/tauntaun.jpg');
-new Item('unicorn', './img/unicorn.jpg');
-new Item('usb', './img/usb.gif');
+new Item('unicorn meat', './img/unicorn.jpg');
+new Item('wiggle usb', './img/usb.gif');
 new Item('water can', './img/water-can.jpg');
 new Item('wine glass', './img/wine-glass.jpg');
-//========================================================================
-//chart
-//=====================================
-
-
-
-console.log(itemLikes, 'these were liked');
-console.log(itemNames, 'the names');
-
-
-
-
-
-
-
